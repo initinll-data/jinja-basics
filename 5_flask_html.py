53bb38a41dd16e9574b36cbead774f87954bc0ba
initinll <initinli@yahoo.com>
2023-09-05 13:50:10 +0530

    added jinja macro example

--- 5_flask_html.py
+++ 5_flask_html.py
@@ -5,7 +5,7 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def hello():
-    return flask.render_template("child.html", title="Maria's Server", body='body content')
+    return flask.render_template("child.html", title="Maria's Server", body='body content', name='Dan')
 
 
 if __name__ == "__main__":
